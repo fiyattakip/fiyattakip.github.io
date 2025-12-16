006cad0aa3182fc0fe09e79157fc4563ae783ed0
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 23:25:10 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  query,
+  orderBy,
+  limit
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcXkVFQzB2XtxO7wqnbXhzM1Io54zCsBI",
@@ -9,8 +29,22 @@ const firebaseConfig = {
   storageBucket: "fiyattakip-ttoxub.firebasestorage.app",
   messagingSenderId: "105868725844",
   appId: "1:105868725844:web:fc04f5a08e708916e727c1",
+  measurementId: "G-M6JXDZ3PK0"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// re-export helpers
+export const fb = {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+
+  doc, setDoc, getDoc,
+  collection, addDoc, getDocs, deleteDoc,
+  query, orderBy, limit
+};
