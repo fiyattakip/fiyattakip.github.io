030fbc32aac0b569c2605eb6ae23ec73ab723d18
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-17 00:34:11 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "fiyattakip-v9"; // her değişiklikte v10, v11 yap
+const CACHE_VERSION = "fiyattakip-v12";
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
@@ -31,7 +31,6 @@ self.addEventListener("fetch", (event) => {
   const req = event.request;
   const url = new URL(req.url);
 
-  // Firebase/CDN gibi dış kaynakları cacheleme (sorun çıkarmasın)
   if (url.origin !== location.origin) return;
 
   event.respondWith((async () => {
@@ -44,7 +43,6 @@ self.addEventListener("fetch", (event) => {
       cache.put(req, res.clone());
       return res;
     } catch (e) {
-      // offline fallback
       if (req.mode === "navigate") {
         return caches.match("./index.html");
       }
