c6c6e1de0c0fb9dcc14acbb4528225b52de33308
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-22 12:00:16 +0300

    firebase.js
    Updated Firebase configuration with placeholder values and added messaging and functions imports.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-functions.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcXkVFQzB2XtxO7wqnbXhzM1Io54zCsBI",
-  authDomain: "fiyattakip-ttoxub.firebaseapp.com",
-  projectId: "fiyattakip-ttoxub",
-  storageBucket: "fiyattakip-ttoxub.firebasestorage.app",
-  messagingSenderId: "105868725844",
-  appId: "1:105868725844:web:fc04f5a08e708916e727c1",
-  measurementId: "G-M6JXDZ3PK0"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const messaging = getMessaging(app);
+export const functions = getFunctions(app);
 export const googleProvider = new GoogleAuthProvider();
