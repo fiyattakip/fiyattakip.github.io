608986bd9b81f6a7d418b5a698ec5404a22e6f92
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-23 13:16:31 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fiyattakip-cache-v4";
+const CACHE = "fiyattakip-cache-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -40,5 +40,3 @@ self.addEventListener("fetch", (e)=>{
     }
   })());
 });
-
-// cache bump v9
