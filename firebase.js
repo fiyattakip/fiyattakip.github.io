602ef8ebb228237034dd50e4caa786d664a1dfb3
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-23 10:37:21 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -2,20 +2,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBcXkVFQzB2XtxO7wqnbXhzM1Io54zCsBI",
   authDomain: "fiyattakip-ttoxub.firebaseapp.com",
   projectId: "fiyattakip-ttoxub",
   storageBucket: "fiyattakip-ttoxub.firebasestorage.app",
   messagingSenderId: "105868725844",
   appId: "1:105868725844:web:fc04f5a08e708916e727c1",
+  measurementId: "G-M6JXDZ3PK0"
 };
+
+export function firebaseConfigLooksInvalid(){
+  return !firebaseConfig?.apiKey || !firebaseConfig?.projectId;
+}
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-
-export function firebaseConfigLooksInvalid() {
-  return !firebaseConfig.apiKey || firebaseConfig.apiKey.includes("PASTE_");
-}
