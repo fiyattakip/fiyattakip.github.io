59b0fd5b8710d19d9a819eae101904f729dfba63
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-28 23:12:47 +0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,22 @@
-/* unchanged: keep your repo version of firebase-messaging-sw.js */
+importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload?.notification?.title || "fiyattakip";
+  const body = payload?.notification?.body || "";
+  self.registration.showNotification(title, {
+    body,
+    icon: "./icon-192.png"
+  });
+});
